c9ed5f7931797547888f89ed2a22182172a72ec9
Urooj Shah <60300832@udst.edu.qa>
2025-11-11 21:13:49 +0300

    adding structure, notebook files

--- fabric/connect_curated_reviews.m
+++
@@ -1,8 +0,0 @@
-let
-    Source = AzureStorage.DataLake(
-        "https://goodreadsreviews60300832.dfs.core.windows.net/lakehouse/gold/curated_reviews/",
-        [HierarchicalNavigation = true]
-    ),
-    DeltaTable = DeltaLake.Table(Source)
-in
-    DeltaTable
--- sql/curated_reviews_registration.sql
+++
@@ -1,9 +0,0 @@
--- Create curated_reviews managed Delta table in the Hive Metastore
--- I did this part in the notebook but wanted to show it here to shwo any sql queires ive done
-DROP TABLE IF EXISTS hive_metastore.default.curated_reviews;
-
-CREATE TABLE hive_metastore.default.curated_reviews
-USING DELTA
-AS SELECT * FROM delta.`/lakehouse/gold/curated_reviews`;
-
-
